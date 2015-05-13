6a90f5eb8fac99caa2877d6fbd70bc1aab44ea94
Caldwerl <caldwerl@gmail.com>
2015-05-13 02:30:40 -0400

    Initial commit with working chat application

---
+++ index.js
@@ -0,0 +1,111 @@
+var socket = io();
+
+var $name = $('#chat-name');
+var $chat = $('#chat-display');
+var $msg = $('#chat-message');
+var $nameList = $('#chat-name-list');
+var $status = $('#chat-status');
+var $statusTxt = $('#chat-status span');
+var $nameForm = $('#name-form');
+var $msgForm = $('#msg-form');
+var $countTxt = $('#chat-count span');
+var whitespacePattern = /^\s*$/;
+var statusOld = $statusTxt.text();
+var alertOld = "alert-info";
+var alertCur;
+var nameOld = "Unknown";
+
+$msgForm.submit(function () {
+
+  if (!whitespacePattern.test($msg.val())) {
+
+    socket.emit('chat message', ({name: $name.val(), message: $msg.val()}));
+  }
+
+  $msg.val('');
+
+  return false;
+});
+
+$nameForm.submit(function () {
+
+  socket.emit('name change', (whitespacePattern.test($name.val()) ? {name: 'Unknown'} : {name: $name.val(), oldName: nameOld, socketID: socket.id}));
+  nameOld = $name.val();
+
+  return false;
+});
+
+
+
+socket.on('populate names', function (names) {
+
+  names.forEach(function(name) {
+
+    $nameList.append($('<li>').text(name));
+  });
+});
+
+
+
+socket.on('name change', function (data) {
+
+    $('#chat-name-list li:contains(' + data.oldName + ')').filter(':first').text(data.name);
+});
+
+
+
+socket.on('chat message', function (data){
+
+  if(data.length != undefined){
+
+    for (var i = 0; i < data.length; i++) {
+
+      $chat.prepend($('<li>').text(data[i].name + ": " + data[i].message));
+    }
+  } else if (data.message != undefined) {
+
+    $chat.prepend($('<li>').text(data.name + ": " + data.message));
+  }
+});
+
+
+
+socket.on('add user', function (name) {
+
+  $nameList.append($('<li>').text(name));
+});
+
+
+
+socket.on('status', function (data) {
+
+  alertCur = data.category;
+
+  $status.toggleClass(alertOld).toggleClass(alertCur);
+  $statusTxt.text(data.message);
+
+  setTimeout(function () {
+
+    $status.toggleClass(alertOld).toggleClass(alertCur);
+    $statusTxt.text(statusOld);
+  }, 5000);
+});
+
+
+
+socket.on('conn update', function (data) {
+
+  $countTxt.text(data);
+});
+
+
+
+
+socket.on('disconnect', function(name) {
+
+  if (name != 'transport close') {
+
+    $chat.append($('<li>').text("Server: " + name + " has disconnected."));
+    $('#chat-name-list li:contains(' + name + ')').filter(':first').remove();
+  }
+});
---
+++ server.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var favicon = require('serve-favicon');
+var mongo = require('mongodb').MongoClient;
+var io = require('socket.io')(server);
+var port = 8080;
+
+var userList = [];
+
+var welcomeMsg = "Welcome to the Empty Datacube. Messages below this are old.";
+
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(express.static(__dirname));
+
+app.get('/', function (request, response) {
+
+  response.sendfile('./index.html');
+});
+
+mongo.connect('mongodb://localhost/chat', function (err, db) {
+
+  if (err) {
+    throw err;
+  }
+
+  io.on('connection', function (socket) {
+
+    var collection = db.collection('chatLog');
+
+    //All new clients are intialized with a name of 'Unknown'
+    //Push new client name to userList and initialize this clients userIndex
+    userList.push('Unknown');
+    var userIndex = userList.length - 1;
+
+    //Function for setting the status alert
+    var sendStatus = function (data) {
+      socket.emit('status', data);
+    };
+
+    //Function for updating the number of connected clients
+    var sendConn = function (data) {
+      socket.emit('conn update', data);
+    };
+
+
+
+    //Populate connected users
+    io.to(socket.id).emit('populate names', userList);
+    sendConn(userList.length);
+
+
+
+    //Populate chat history
+    collection.find().limit(50).toArray(function (err, response) {
+
+      if (err) {
+        sendStatus({category: "alert-danger", message: 'Error fetching messages'});
+      }
+
+      //Populates top 50 chatLog history and welcome message only to new client
+      io.to(socket.id).emit('chat message', response);
+      io.to(socket.id).emit('chat message', {name: 'Server', message: welcomeMsg});
+    });
+
+    //Sends connection message and adds new client to every other client
+    socket.broadcast.emit('chat message', {name: userList[userIndex], message: "has connected."});
+    socket.broadcast.emit('add user', userList[userIndex]);
+
+
+    //Logic for changing user's name
+    socket.on('name change', function(data) {
+
+      io.emit('chat message', {name: "Server", message: data.oldName + " is now known as " + data.name});
+      io.emit('name change', data);
+      sendStatus({category: "alert-success", message: 'Name changed'});
+
+      if (userList.indexOf(data.oldName) >= 0) {
+
+        userList[userList.indexOf(data.oldName)] = data.name;
+      }
+    });
+
+
+    //Logic for inserting into the db and sending a new message
+    socket.on('chat message', function (data) {
+
+      collection.insert({socketID: socket.id, name: data.name, message: data.message, date: new Date().toString()});
+      io.emit('chat message', {name: data.name, message: data.message});
+      sendStatus({category: "alert-success", message: 'Message sent'});
+    });
+
+
+    //Logic for disconnect, removes user from userList and updates connection counter
+    socket.on('disconnect', function () {
+
+      io.emit('disconnect', userList[userIndex]);
+      userList.splice(userIndex, 1);
+      sendConn(userList.length);
+    });
+  });
+});
+
+
+server.listen(port, function() {
+
+  console.log('Server now listening on port ', port);
+});
